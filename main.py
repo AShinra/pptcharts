1e25d2ff1ef6116df9544a2c0593e37108af0645
Athan <jonpuray@gmail.com>
2025-03-04 12:20:36 +0800

    updated

--- main.py
+++ main.py
@@ -39,13 +39,14 @@ def load_ppt_template():
 if __name__ == '__main__':
     
     # load data
-    csv_file = st.file_uploader('Upload File', type=['csv'])
-    if csv_file:
+    csv_file = st.file_uploader('Upload File', type=['csv'], key='file_csv')
+    if st.session_state['file_csv'] is in ['None', '']:
+        st.write('NONE')
+    else:
         df = get_data_from_csv(csv_file)
         with st.expander('VIEW DATAFRAME'):
             st.dataframe(df)
-    else:
-        pass
+    
     exit()
     # load template
     col11, col12 = st.columns([0.25, 0.75])
