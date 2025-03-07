1ac41bb1afa9f435672077fc999757d92d6141cd
Athan <jonpuray@gmail.com>
2025-03-07 22:13:01 +0800

    updated

--- charts_bar.py
+++ charts_bar.py
@@ -194,18 +194,20 @@ def add_bar_slide(df, prs, grouping, bar_orientation):
         with col_legend12:
             cht_legend_dict['font_size'] = st.number_input('Font Size', min_value=1, max_value=100, value=10, step=1, key='cht_legend_font_size')
         with col_legend13:
+            'ddd'
+
+        col_legend21, col_legend22, col_legend23, col_legend24 = st.columns(4)
+        with col_legend21:
             if st.checkbox('Bold', key='cht_legend_bold'):
                 cht_legend_dict['bold'] = True
             else:
                 cht_legend_dict['bold'] = False
-        
-        col_legend21, col_legend22, col_legend23, col_legend24 = st.columns(4)
-        with col_legend21:
+        with col_legend22:
             if st.checkbox('Italic', key='cht_legend_italic'):
                 cht_legend_dict['italic'] = True
             else:
                 cht_legend_dict['italic'] = False 
-        with col_legend22:
+        with col_legend23:
             legend_loc = st.selectbox('Location', options=['Bottom', 'Top', 'Corner', 'Left', 'Right'])
             if legend_loc == 'Bottom':
                 cht_legend_dict['location'] = XL_LEGEND_POSITION.BOTTOM
@@ -217,7 +219,7 @@ def add_bar_slide(df, prs, grouping, bar_orientation):
                 cht_legend_dict['location'] = XL_LEGEND_POSITION.LEFT
             elif legend_loc == 'Right':
                 cht_legend_dict['location'] = XL_LEGEND_POSITION.RIGHT
-        with col_legend23:
+        
             legend_bool = st.checkbox('Show')
             if legend_bool:
                 cht_legend_dict['visible'] = True
